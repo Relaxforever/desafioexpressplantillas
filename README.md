a1f8f0ee5916e802b74cf6a5b6586bb7a741803f
Fidel Caicedo <fidelcaicedo226@hotmail.com>
2022-07-12 21:22:53 -0500

    subiendo archivos del desafio de express 3

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # desafioexpressplantillas
+
+# Ejecutar dentro de cada carpeta cada proyecto ya se 
+
+``` 
+con node o nodemon
+DigitalHouse\desafio-express3> cd pug
+DigitalHouse\desafio-express3\pug> nodemon .\index.js   
+ ```
--- pug/index.js
+++ pug/index.js
@@ -11,53 +11,31 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 
-app.use('/api/productos', router);
 app.use(express.static('public'))
-router.use(express.json())
-router.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
 const PORT = process.env.PORT || 8080;
 
-router.get('/productos', (req, res) => {
+app.get('/productos', (req, res) => {
     res.render('hello.pug', {mensaje: 'Usando Pug JS en Express'});
 });
 
 
-router.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
     const arrayAll = await ContenedorEx.getAll()
     res.render('getProductos.pug', {results: arrayAll})
     //return res.send( arrayAll)
 })
 
-router.get('/:id', async (req, res) => {
-    const RetrievedObj =  await ContenedorEx.getById(parseInt(req.params.id))
-    if (RetrievedObj) {
-       return  res.send(RetrievedObj)
-    }else {
-       return  res.send({"error": "Producto no Encontrado"})
-    }
-})
-
-
-router.post('/', async (req, res) => {
+app.post('/productos', async (req, res) => {
     console.log(req.body)
     await ContenedorEx.save(req.body)
     const arrayAll = await ContenedorEx.getAll()
     res.render('getProductos.pug', {results: arrayAll})
 
 })
-
-router.put('/:id', async (req, res) => {
-    const updatedObject = await ContenedorEx.updateById(parseInt(req.params.id), req.body)
-    return res.send(updatedObject)
-})
-
-router.delete('/:id', async (req, res) => {
-    await ContenedorEx.deleteById(parseInt(req.params.id))
-    return res.sendStatus(200)
-})
-
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
